2fd53d64e8c35ee6f11c75d182d76ffe9dceb217
jotapesc <jpsc081204@gmail.com>
2026-04-15 10:58:10 -0300

    feat: integrate PrismaClient and create test user

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 export const app = fastify();
+export const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Kadu",
+    email: "scotthopkins666@gmail.com",
+  },
+});
